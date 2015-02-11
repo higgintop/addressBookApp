e7035d949ca91934eda404164cfadd2dc5a68972
Allison Higginbotham <higgintop@gmail.com>
2015-02-11 15:36:51 -0600

    Added a lot of things

--- karma.conf.js
+++ karma.conf.js
@@ -15,9 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/firebase/firebase.js',
-      'bower_components/jquery/dist/jquery.min.js',
-      'bower_components/lodash/lodash.min.js',
+      'public/vendor/**/*.js',
       'public/js/**/*.js',
       'test/spec/**/*.test.js'
     ],
